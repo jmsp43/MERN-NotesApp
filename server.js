e47bc040e6bcf3d86a097dcb2c27ad6d480513b0
Jenna Perez <jmsp43@gmail.com>
2023-05-24 17:30:00 -0400

    added logger middleware

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.port || 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const PORT = process.env.port || 3000;
 
-//telling express where to find static files like css or images
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger);
+//lets our app recieve and parse json data
+app.use(express.json());
 
-app.use('/', require('./routes/root'))
+//telling server where to find static files like css or images
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.all('*', (req, res) => {
-     res.status(404)
-     if (req.accepts('html')) {
-          res.sendFile(path.join(__dirname, 'views', '404.html'))
-     } else if (req.accepts('json')) {
-          res.json({message: '404 Not Found'})
-     } else {
-          res.type('text').send('404 Not Found')
-     }
-})
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "/views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("text").send("404 Not Found");
+  }
+});
 
 app.listen(PORT, () => {
-     console.log(`running on port ${PORT}`)
-})
+  console.log(`running on port ${PORT}`);
+});
