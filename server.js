294ab58bc5a7e8c489602f2c5316f879c72f6fcc
Jenna Perez <jmsp43@gmail.com>
2023-05-25 08:58:50 -0400

    added 3rd party cookie parser middleware

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+
 const PORT = process.env.port || 3000;
 
 app.use(logger);
 //lets our app recieve and parse json data
 app.use(express.json());
+
+//now app will be able to parse cookies
+app.use(cookieParser())
 
 //telling server where to find static files like css or images
 app.use("/", express.static(path.join(__dirname, "/public")));
